5374f51541af8b79d024ea9538f865f3071b6f3b
olishchuk1 <30534182+olishchuk1@users.noreply.github.com>
2019-09-03 23:31:35 +0300

    New methods added

--- pymatrix.py
+++ pymatrix.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 class Pymatrix:
     def __init__(self, matrix):
         self.matrix = matrix
@@ -13,24 +10,52 @@ class Pymatrix:
         pass
 
     def mmultiply(self, matrix):
-        rows = len(self.matrix)
-        cols = len(matrix[0])
-        result_matrix = [[0 for i in range(cols)] for i in range(rows)]
-        # try:
-        #     for i in range(len(self.matrix)):
-        #         for j in range(len(self.matrix[0])):
-        #
-        return result_matrix
+        try:
+            result_matrix = [[0 for row in range(len(self.matrix))] for col in range(len(matrix[0]))]
+            for i in range(len(self.matrix)):
+                for j in range(len(matrix[0])):
+                    for k in range(len(matrix)):
+                        result_matrix[i][j] += self.matrix[i][k] * matrix[k][j]
+            self.matrix = result_matrix
+        except IndexError:
+            pass
+        pass
 
-    # def rotate(self, turn_number):
-    #     turn_number = turn_number % 4
-    #     for i in self.matrix:
-    #         for j in self.matrix[i]:
+    def maddition(self, matrix):
+        try:
+            result_matrix = [[0 for col in range(len(matrix[0]))] for row in range(len(matrix))]
+            for i in range(len(matrix)):
+                for j in range(len(matrix[0])):
+                    result_matrix[i][j] = self.matrix[i][j] + matrix[i][j]
+            self.matrix = result_matrix
+        except IndexError:
+            pass
+        pass
+
+    def mtranpose(self):
+        result_matrix = [[0 for col in range(len(self.matrix))] for row in range(len(self.matrix[0]))]
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                result_matrix[j][i] = self.matrix[i][j]
+        self.matrix = result_matrix
+        pass
+
+    def mrotate(self):
+        result_matrix = [[0 for col in range(len(self.matrix[0]))] for row in range(len(self.matrix))]
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                result_matrix[i][j] = self.matrix[i][len(self.matrix[0]) - 1 - j]
+                # left turn -> result_matrix[i][j] = self.matrix[len(self.matrix) - 1 - i][j]
+        self.matrix = result_matrix
+        pass
 
 
 def main():
     mat = Pymatrix([[1, 2, 3], [6, 5, 4]])
-    pprint(mat.mmultiply([[1, 3], [1, 5], [4, 2]]))
+    mat.maddition([[1, 3, 5], [4, 2, 3]])
+    mat.mprint()
+    mat.mtranpose()
+    mat.mrotate()
     mat.mprint()
 
 
