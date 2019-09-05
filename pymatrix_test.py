65636283ed37004e3ff57fa7a953cfdabc0b1a8b
olishchuk1 <30534182+olishchuk1@users.noreply.github.com>
2019-09-05 14:12:14 +0300

    gaussian elimination implemented

--- pymatrix_test.py
+++ pymatrix_test.py
@@ -8,12 +8,11 @@ class PymatrixTest(unittest.TestCase):
         self.assertEqual(True, True)
 
     # NEED TO BE FIXED
-    def madition_test(self):
+    def test_madition(self):
         mat = Pymatrix([[1, 2, 3], [6, 5, 4]])
-        mat.maddition([[1, 3, 5], [4, 2, 3]])
-        self.assertEqual(mat, [[2, 5, 8], [10, 7, 7]])
+        self.assertEqual(mat.maddition([[1, 3, 5], [4, 2, 3]]), [[2, 5, 8], [10, 7, 7]])
 
-    def mmultiply_test(self):
+    def test_mmultiply(self):
         mat = Pymatrix([[1, 2], [3, 4], [5, 6]])
         self.assertEqual(mat.mmultiply([[2, 6, 5], [1, 8, 3]]), [[4, 22, 11], [10, 50, 27], [16, 78, 43]])
 
--- sole.py
+++ sole.py
@@ -6,8 +6,28 @@ class Sole(Pymatrix):
         self.matrix = matrix
         self.column_vector = column_vector
 
-    def gaussian_elimination(self):
-        pass
+    def gaussian_elimination(self, matrix):
+        # eliminate columns
+        for col in range(len(matrix[0])):
+            for row in range(col + 1, len(matrix)):
+                r = [(row_value * (-(matrix[row][col] / matrix[col][col]))) for row_value in matrix[col]]
+                matrix[row] = [sum(pair) for pair in zip(matrix[row], r)]
+        # now backsolve by substitution
+        result = []
+        matrix.reverse()  # makes it easier to backsolve
+        for row in range(len(matrix)):
+            if row == 0:
+                result.append(matrix[row][-1] / matrix[row][-2])
+            else:
+                inner = 0
+                # substitute in all known coefficients
+                for column in range(row):
+                    inner += (result[column] * matrix[row][-2 - column])
+                # the equation is now reduced to ax + b = c form
+                # solve with (c - b) / a
+                result.append((matrix[row][-1] - inner) / matrix[row][-row - 2])
+        result.reverse()
+        return result
 
     def gauss_jordan_elimination(self):
         pass
